54b4fa30de573e479a6f41566d7e408642ddad39
Kim Jun Ho <rlawnsgh933@naver.com>
2022-09-21 20:17:58 +0900

    환경설정 작업

--- djangomission/djangomission/settings.py
+++ djangomission/djangomission/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'contentshub',
     'community',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -103,12 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
+# Internationaslization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
